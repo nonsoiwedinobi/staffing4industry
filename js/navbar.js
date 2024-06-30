29b80fb525a7cf5c875925db31d72e3142523d94
Nonso Iwedinobi <nonsoiwedinobi@yahoo.com>
2024-06-30 22:10:14 +0100

    added call back section

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,23 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("toggle_btn");
+  const navLinks = document.getElementById("nav-links");
+  const toggleIcon = toggleBtn.querySelector("i");
+
+  toggleBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+
+    // Toggle between bars and close icons
+    if (navLinks.classList.contains("active")) {
+      toggleIcon.classList.remove("fa-bars");
+      toggleIcon.classList.add("fa-times");
+    } else {
+      toggleIcon.classList.remove("fa-times");
+      toggleIcon.classList.add("fa-bars");
+    }
+  });
+});
+
