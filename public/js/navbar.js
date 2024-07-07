d3d801b957c4df257e9816b7d9fc47cce7e1e03b
Nonso Iwedinobi <nonsoiwedinobi@yahoo.com>
2024-07-07 22:30:36 +0100

    Added dropdown menu

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.getElementById("nav-links");
   const toggleIcon = toggleBtn.querySelector("i");
   const navbar = document.getElementById("navbar");
+  const langBtn = document.getElementById("lang-btn");
+  const dropdown = document.getElementById("language-dropdown");
+  const arrowIcon = document.getElementById("arrow-icon");
 
   // Handle Navbar toggle button click
   toggleBtn.addEventListener("click", function () {
@@ -19,9 +22,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Handle Language dropdown toggle
-  document.getElementById("lang-btn").addEventListener("click", function () {
-    const dropdown = document.getElementById("language-dropdown");
-    const arrowIcon = document.getElementById("arrow-icon");
+  langBtn.addEventListener("click", function (e) {
+    e.stopPropagation(); // Prevent the click event from bubbling up to the document
     if (dropdown.style.display === "block") {
       dropdown.style.display = "none";
       arrowIcon.innerHTML = "&#9660;";
@@ -32,28 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Handle Language selection
-  document
-    .getElementById("language-dropdown")
-    .addEventListener("click", async function (e) {
-      if (e.target.tagName === "LI" || e.target.tagName === "IMG") {
-        const selectedElement = e.target.closest("li");
-        const selectedLanguage = selectedElement.getAttribute("data-lang");
-        const selectedText = selectedElement.textContent.trim();
-        const flagSrc = selectedElement.querySelector(".flag-icon").src;
+  dropdown.addEventListener("click", async function (e) {
+    if (e.target.tagName === "LI" || e.target.tagName === "IMG") {
+      const selectedElement = e.target.closest("li");
+      const selectedLanguage = selectedElement.getAttribute("data-lang");
+      const selectedText = selectedElement.textContent.trim();
+      const flagSrc = selectedElement.querySelector(".flag-icon").src;
 
-        document.getElementById("lang-btn").innerHTML =
-          '<img src="' +
-          flagSrc +
-          '" alt="' +
-          selectedText +
-          '" class="flag-icon"> ' +
-          selectedText +
-          ' <span id="arrow-icon" class="arrow-icon">&#9660;</span>';
-        document.getElementById("language-dropdown").style.display = "none";
+      langBtn.innerHTML =
+        '<img src="' +
+        flagSrc +
+        '" alt="' +
+        selectedText +
+        '" class="flag-icon"> ' +
+        selectedText +
+        ' <span id="arrow-icon" class="arrow-icon">&#9660;</span>';
+      dropdown.style.display = "none";
 
-        await translatePage(selectedLanguage);
-      }
-    });
+      await translatePage(selectedLanguage);
+    }
+  });
 
   // Function to handle adding/removing dark background on navbar
   function handleNavbarScroll() {
@@ -69,6 +69,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Handle page scroll to add/remove dark background on navbar
   window.addEventListener("scroll", handleNavbarScroll);
+
+  // Close dropdown if clicked outside
+  document.addEventListener("click", function (e) {
+    if (!dropdown.contains(e.target) && !langBtn.contains(e.target)) {
+      dropdown.style.display = "none";
+      arrowIcon.innerHTML = "&#9660;";
+    }
+  });
 });
 
 // Translation function for Netlify function implementation
